4bf4ea2bc211cce349353917d02ceab01431aa97
Naveen <snaveen100@yahoo.com>
2021-09-01 11:40:53 +0530

    Initial commit

---
+++ src/main/java/com/first/gradle/myGradleProject/Controller/Controller.java
@@ -0,0 +1,24 @@
+package com.first.gradle.myGradleProject.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Controller {
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@GetMapping("/")
+	public String getGitApi() {
+		return restTemplate.getForObject("https://api.github.com/users/Zen", String.class);
+	}
+
+	@GetMapping("/greeting")
+	public String getGreeting(){
+		return "Hello World";
+	}
+
+}
